cf28f4c73b3e9ccad051c7cc0610410510adac4c
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-11 01:36:34 +0600

    final

---
+++ cart.js
@@ -0,0 +1,35 @@
+const addProduct = () => {
+    const productField= document.getElementById('product-name').value
+    const quantityField = document.getElementById('product-quantity').value
+    // productField.value = ''
+    // quantityField.value=''
+    console.log(productField,quantityField)
+    showProduct(productField, quantityField)
+    saveProductTOLocalStorage(productField, quantityField)
+}
+const showProduct = (productField, quantityField) => {
+    const ul= document.getElementById('product-container')
+    const li = document.createElement('li')
+    li.innerText = `${productField}: ${quantityField}`
+    ul.appendChild(li)
+}
+const getSToredShoppingCart = () => {
+    let  cart= {}
+    const storedCard = localStorage.getItem('cart')
+    if (storedCard) {
+        cart= JSON.parse(storedCard)
+    }
+    return cart
+}
+const saveProductTOLocalStorage = (productField, quantityField) => {
+    const cart = getSToredShoppingCart()
+    cart[productField] = quantityField
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('cart',cartStringified)
+}
+
+const displayProductsFromSLocalStorage = () => {
+    const savedCart = getSToredShoppingCart()
+    
+}
+displayProductsFromSLocalStorage()
---
+++ promt.js
@@ -0,0 +1,14 @@
+// alert('how are you')
+function showAlert() { 
+    alert('bondhu tk dhar de')
+}
+const tkdhar = () => {
+    const result = confirm('ok done')
+    console.log(result)
+    if (result === true) {
+        console.log('you are my best friend')
+    }
+    else {
+        console.log('DGM<<<<<<')
+    }
+}
